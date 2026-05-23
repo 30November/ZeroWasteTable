68639c9b52ab82a2149c072342b68f6a8a4f1a38
30November <biswayanmal@gmail.com>
2026-05-23 15:59:23 +0530

    Made Change

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask, render_template, request, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+from datetime import datetime
 
 app =  Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] =  False
+
+db = SQLAlchemy(app)
+
+#User(user_id, name, email, password_hash, phone, role, is_verified, created_at)
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    phone = db.Column(db.String(15), unique=True, nullable=True)
+    role = db.Column(db.String(50), nullable=False, default='USER')
+    is_verified = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=func.now)
+   
+
 
 @app.route("/")
 def home():
@@ -36,9 +56,10 @@ def rLogin():
 def rRegister():
     return render_template("auth/restaurant-register.html")
 
-# GATEWAY PATH
 
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True,host='0.0.0.0')
